44e12715ceb972a3c782364b1e0deca14c29772b
csm-kr <csm8167@naver.com>
2020-08-14 14:48:14 +0900

    update models

--- model.py
+++ model.py
@@ -15,26 +15,26 @@ class EmbeddingNet(nn.Module):
                                      nn.ReLU(inplace=True),
                                      nn.BatchNorm2d(32),
 
-                                     # nn.Conv2d(32, 32, 3, padding=1),  # 6
-                                     # nn.ReLU(inplace=True),
-                                     # nn.BatchNorm2d(32),
+                                     nn.Conv2d(32, 32, 3, padding=1),  # 6
+                                     nn.ReLU(inplace=True),
+                                     nn.BatchNorm2d(32),
 
                                      nn.Conv2d(32, 64, 3, padding=1),  # 6
                                      nn.ReLU(inplace=True),
                                      nn.BatchNorm2d(64),
 
-                                     # nn.Conv2d(64, 64, 3, padding=1),  # 6
-                                     # nn.ReLU(inplace=True),
-                                     # nn.BatchNorm2d(64),
+                                     nn.Conv2d(64, 64, 3, padding=1),  # 6
+                                     nn.ReLU(inplace=True),
+                                     nn.BatchNorm2d(64),
                                      nn.MaxPool2d(2, stride=2),  # 3
 
                                      nn.Conv2d(64, 128, 3, padding=1),  # 3
                                      nn.ReLU(inplace=True),
                                      nn.BatchNorm2d(128),
 
-                                     # nn.Conv2d(128, 128, 3, padding=1),  # 3
-                                     # nn.ReLU(inplace=True),
-                                     # nn.BatchNorm2d(128),
+                                     nn.Conv2d(128, 128, 3, padding=1),  # 3
+                                     nn.ReLU(inplace=True),
+                                     nn.BatchNorm2d(128),
                                      )
 
         self.fc = nn.Sequential(nn.Linear(128 * 7 * 7, 128),
